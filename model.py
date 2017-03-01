64115d9a63f56f72f87e2ddbf32ac65c391460d5
sireeshreddy <sireesh.reddy@gmail.com>
2017-03-01 01:15:54 -0800

    Update model.py

--- model.py
+++ model.py
@@ -102,7 +102,7 @@ model.summary()
 # compile model using mean standard error and Adam optimizer
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, 
-                    samples_per_epoch = len(train_samples) * 8, 
+                    samples_per_epoch = len(train_samples) * 7, 
                     nb_epoch = 15,
                     validation_data = (validation_generator),              
                     nb_val_samples = len(validation_samples))
